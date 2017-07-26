45a593059dfc9ede91e169b1f2ecb270d976c965
Vadim Goncharov <vg@vadimgoncharov.ru>
2017-07-26 17:37:07 +0300

    Initial Commit

---
+++ RestorePicnicAfishaContest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestorePicnicAfishaContest
+//
+//  Created by Vadim Goncharov on 26/07/2017.
+//  Copyright © 2017 Vadim Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
